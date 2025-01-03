4f9c3d9139fc358b93c124548d5360edc731b994
Marcel Frey <marcelfrey29@mailbox.org>
2025-01-03 15:00:38 +0100

    chore(cloudformation): add Lambda function

--- cloudformation/deploy-backend.sh
+++ cloudformation/deploy-backend.sh
@@ -1,7 +1,8 @@
 echo "Uploading Lambda Code to S3..."
 
-(cd ../backend && GOOS=linux GOARCH=amd64 go build -o bootstrap)
-zip ./code/lambda-handler.zip ../backend/bootstrap
+(cd ../backend && GOOS=linux GOARCH=arm64 go build -o bootstrap)
+(cd ../backend && zip lambda-handler.zip bootstrap)
+cp ../backend/lambda-handler.zip code/lambda-handler.zip
 aws s3 sync ./code s3://$(aws cloudformation describe-stacks --stack-name TravelGuides --query 'Stacks[0].Outputs[?OutputKey==`DeploymentBucketName`].OutputValue' --output text)
 
 echo "Upload of Lambda Function Code to S3 complete."
--- cloudformation/update-stack.sh
+++ cloudformation/update-stack.sh
@@ -2,6 +2,6 @@
 
 echo "Updating CloudFormation Stack..."
 
-aws cloudformation update-stack --stack-name TravelGuides --template-body file://template.yaml
+aws cloudformation update-stack --stack-name TravelGuides --template-body file://template.yaml --capabilities CAPABILITY_IAM
 
 echo "Updating CloudFormation Stack."
