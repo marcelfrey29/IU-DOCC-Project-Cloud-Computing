811dfd26ab0be0e6a7ffe1c544bb2bc706272b79
Marcel Frey <marcelfrey29@mailbox.org>
2025-01-03 15:58:45 +0100

    chore: build web app before deployment and add single update script

--- cloudformation/deploy-web-app.sh
+++ cloudformation/deploy-web-app.sh
@@ -1,5 +1,6 @@
 echo "Deploying Web App to S3..."
 
+(cd ../web-app && npm install --frozen-lockfile && npm run build:cloud)
 aws s3 sync ../web-app/dist s3://$(aws cloudformation describe-stacks --stack-name TravelGuides --query 'Stacks[0].Outputs[?OutputKey==`WebAppBucketName`].OutputValue' --output text)
 
 echo "Deployment of Web App complete."
